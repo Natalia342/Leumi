149e6a7a2f4f1fbb799256fbd554da68250a47dc
Natalia342 <nataliatalova7@gmail.com>
2023-03-13 14:27:55 +0200

    HW_10

--- src/test/java/FindElement.java
+++ src/test/java/FindElement.java
@@ -7,6 +7,7 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class FindElement {
     WebDriver wd;
@@ -15,12 +16,30 @@ public class FindElement {
     public void preCondition() {
         wd = new ChromeDriver();
         wd.navigate().to("https://www.leumi.co.il/#Home");
+        wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
     @Test
     public void testFind(){
+     //  WebElement element1 = wd.findElement(By.xpath("//*[text()='פרטי']"));
+     //   System.out.println(element1);
         List<WebElement> elements = wd.findElements(By.cssSelector("li.first>a[title='פרטי']"));
-        System.out.println(elements.size());
+ //       for(WebElement el:elements){
+ //           System.out.println(el.getText());
+ //       }
+        System.out.println(elements);
 
+ //    WebElement element = wd.findElement(By.cssSelector("li.first>a[title='פרטי']"));
+ //       System.out.println(element.getText());
+    }
+    public void pause(int time){
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+//        FluentWait<WebDriver> wait = new FluentWait<>(wd);
+//        wait.withTimeout(Duration.ofSeconds(time));
     }
 
     @AfterMethod
